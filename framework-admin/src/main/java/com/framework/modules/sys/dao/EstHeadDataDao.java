57c661c082c28d13f849936749807a16572e640b
641407168 <641407168@qq.com>
2018-06-27 11:05:22 +0800

    第一版1

---
+++ framework-admin/src/main/java/com/framework/modules/sys/dao/EstHeadDataDao.java
@@ -0,0 +1,15 @@
+package com.framework.modules.sys.dao;
+
+import com.framework.modules.sys.entity.EstHeadDataEntity;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * 
+ * 
+ * @author MIT
+ * @email framework@gmail.com
+ * @date 2018-06-26 14:15:09
+ */
+public interface EstHeadDataDao extends BaseMapper<EstHeadDataEntity> {
+	
+}
